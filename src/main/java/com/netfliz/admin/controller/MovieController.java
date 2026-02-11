922feaeff09908138f155eee198519bc32bc0eb0
sonpt1 <sonpt1@vnpay.vn>
2026-02-11 17:43:30 +0700

    update controller

--- src/main/java/com/netfliz/admin/controller/MovieController.java
+++ src/main/java/com/netfliz/admin/controller/MovieController.java
@@ -4,11 +4,15 @@ import com.netfliz.admin.dto.MovieDto;
 import com.netfliz.admin.dto.request.MovieFilterRequest;
 import com.netfliz.admin.dto.response.PageResponse;
 import com.netfliz.admin.service.MovieService;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -16,33 +20,28 @@ import java.util.List;
 public class MovieController {
     private final MovieService movieService;
 
-    @PostMapping("/filter")
+    @GetMapping("/filter")
     public PageResponse<MovieDto> getMoviesByFilter(MovieFilterRequest request) {
         return movieService.getMoviesByFilter(request);
     }
 
     @GetMapping("/{id}")
-    public MovieDto getMovieById(@PathVariable Long id) {
+    public MovieDto getMovieById(Long id) {
         return movieService.getMovieById(id);
     }
 
-    @PostMapping("")
-    public MovieDto createMovie(@RequestBody @Valid MovieDto request) {
+    @PostMapping()
+    public MovieDto createMovie(@RequestBody MovieDto request) {
         return movieService.createMovie(request);
     }
 
     @PutMapping("/{id}")
-    public MovieDto updateMovie(@PathVariable Long id, @RequestBody @Valid MovieDto request) {
+    public MovieDto updateMovie(@PathVariable Long id, @RequestBody MovieDto request) {
         return movieService.updateMovie(id, request);
     }
 
     @DeleteMapping("/{id}")
     public void deleteMovie(@PathVariable Long id) {
         movieService.deleteMovie(id);
-    }
-
-    @PostMapping("/bulk")
-    public Integer bulkMovie(@RequestBody List<@Valid MovieDto> movies) {
-        return movieService.bulkMovie(movies);
     }
 }
